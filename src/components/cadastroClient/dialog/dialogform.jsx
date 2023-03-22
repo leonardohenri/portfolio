ce85425b867751602f936c6dd7c9a6b33f820ef5
leonardo <leonardohenri2015@outlook.com>
2023-03-22 15:23:13 -0300

    teste-3

--- src/components/cadastroClient/dialog/dialogform.jsx
+++ src/components/cadastroClient/dialog/dialogform.jsx
@@ -1,11 +1,7 @@
 import React, { useState } from "react";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogTitle from "@material-ui/core/DialogTitle";
+
 import Axios from "axios";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from "@mui/material";
 
 
 export default function FormDialog(props) {
