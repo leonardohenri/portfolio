2477190b34e013609d943b25032df718e5f03cda
Leonardo Henrique da Silva <77017309+leonardohenri@users.noreply.github.com>
2023-04-21 12:16:23 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './global.css';
-import { Crud } from './templates/cadastroClient';
 import { Calculator } from './templates/Calculator';
 
-import { Dropdown } from './templates/dropdown';
 import {Faq} from './templates/Faq'
 import { FormPage } from './templates/Form';
 import { Home } from './templates/home';
-import {Homepage} from './templates/Homepage'
 import { Pokedex } from './templates/PokeApi/pokedex';
 import {Qrcode} from './templates/Qrcode'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,17 +17,15 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path='/dropdown' element={<Dropdown/>}/>
         <Route path='/faq' element={<Faq/>}/>
         <Route path='/form' element={<FormPage/>}/>
-        <Route path='/homepage' element={<Homepage/>}/>
         <Route path='/qrcode' element={<Qrcode/>}/>
         <Route path='/calculator' element={<Calculator/>}/>
         <Route path='/pokedex' element={<Pokedex/>}/>
-        <Route path ='/crud' element={<Crud/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
+
 
 
